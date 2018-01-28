6498f8f1aeb13a0da3018e005df3cdbca81ffc66
Vimal-Raghubir <vraghubir0418@gmail.com>
2018-01-28 18:38:06 -0500

    Fixed GET request

--- server.js
+++ server.js
@@ -1,19 +1,28 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var PNF = require('google-libphonenumber').PhoneNumberFormat;
+var phoneUtil = require('google-libphonenumber').PhoneNumberUtil.getInstance();
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 app.get("/", function(req, res) {
+	
 	res.json({message: "HELLO WORLD!"});
 });
 
-app.get("/api/phonenumbers/parse/text/", function(req, res) {
-	res.json({message: "Parsing part for text exists here!"});
+app.get("/api/phonenumbers/parse/text/:number*", function(req, res) {
+	if (req.params.number = "nothing") {
+		res.json({message: "[]"});
+	} else {
+		var phoneNumber = phoneUtil.parse(req.params.number, 'CA');
+		res.json({message: phoneUtil.format(phoneNumber, PNF.NATIONAL)});
+	}
 });
 
-app.post("/api/phonenumbers/parse/file/", function(req, res) {
+app.post("/api/phonenumbers/parse/file/:filename*", function(req, res) {
+	
 	res.json({message: "Parsing part for file exists here!"});
 });
 var port = process.env.PORT || 8080;
